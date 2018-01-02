ee03424cc4a80bb8c455b72a6c24692a1df19dcf
Michelle Dicks <michd@alumni.stanford.edu>
2018-01-02 15:02:17 -0800

    finished problem 1

---
+++ problem1.cpp
@@ -0,0 +1,17 @@
+// Michelle Dicks
+// CS 52
+// Assignment #1
+
+# include <iostream>
+# include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout << "Hello World" << endl;
+	cout << "Type your name: "; 
+	getline(cin, name);
+	cout << "Hello, " << name << endl;
+	return 0;
+}
