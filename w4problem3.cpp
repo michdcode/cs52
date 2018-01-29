8e7973e11241d2fc3a8ab9a477885020044f2f1f
Michelle Dicks <michd@alumni.stanford.edu>
2018-01-28 20:45:45 -0800

    finished problem 3

---
+++ w4problem3.cpp
@@ -0,0 +1,70 @@
+// Michelle Dicks
+// CS 52
+// Assignment #4
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+
+// Employee Structure
+struct Employee
+{
+	string firstName, lastName;
+	int pay;
+};
+
+// Function prototype for printing 
+void printEmployees(Employee *, int);
+
+int main()
+{
+	int n, p; // number of employee variables
+	Employee *usersEmployees; // dynamically allocated array
+	string fn, ln; 
+
+	// Get number of employees
+	cout << "Number of employees: ";
+	cin >> n;
+	usersEmployees = new Employee[n]; // dynamically allocate array 
+
+	// Get the employee information for each employee and add it to the array
+	for (int c = 0; c < n; c++)
+	{
+		cin.ignore();
+		cout << "Employee " << (c + 1) << endl;
+		cout << "First Name: ";
+		getline(cin, fn); 
+		usersEmployees[c].firstName = fn;
+		cout << "Last Name: ";
+		getline(cin, ln);
+		usersEmployees[c].lastName = ln;
+		cout << "Pay: ";
+		cin >> p;
+		usersEmployees[c].pay = p;
+	}
+
+	// Call function that prints elements of array
+	printEmployees(usersEmployees, n);
+
+	// Free dynamically allocated memory
+	delete [] usersEmployees;
+}
+
+/************************************************************************
+*							print Employees								*
+*	This function will print out each of the Employee objects in the 	*
+*	array containing the employee information. 							*
+************************************************************************/
+void printEmployees(Employee *userArray, int num)
+	{
+		cout << endl; 
+		cout << "Employees: " << endl;
+		for (int count = 0; count < num; count ++)
+		{
+			cout << "Name: " << userArray[count].firstName << " ";
+			cout << userArray[count].lastName << " (";
+			cout << userArray[count].pay << ")" << endl;
+		}	
+	}
